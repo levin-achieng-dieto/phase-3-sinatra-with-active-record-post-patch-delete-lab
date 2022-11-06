0f2050b2b22b26dbf004321ef0d1976534f83a29
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-11-06 07:48:47 +0300

    commit tree b2b09b0bc359cb107ea852c708a2121a86ffb4da

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,13 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
 end
